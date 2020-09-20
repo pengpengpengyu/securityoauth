923dfae00fc11e533dcf581b3852a92faa136ccb
wangpengyu <shs-xxaqbzywyp@chinaunicom.cn>
2020-09-20 17:04:06 +0800

    配置JDBC存储端点信息，以及端点权限配置

--- mengxuegu-cloud-oauth2-server/src/main/java/com/mengxuegu/oauth2/config/AuthorizationServerConfig.java
+++ mengxuegu-cloud-oauth2-server/src/main/java/com/mengxuegu/oauth2/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.mengxuegu.oauth2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,7 +10,14 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * @author wangpengyu
@@ -33,6 +41,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private TokenStore tokenStore;
 
+    @Autowired
+    private DataSource dataSource;
+
     /**
      * 配置允许访问此认证服务器的客户端详细信息
      * 授权模式
@@ -46,7 +57,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         // 使用内存方式
 
-        clients.inMemory()
+        /*clients.inMemory()
                 .withClient("mengxuegu-pc")  // 客户端ID
                 // 客户端密码，要加密，不然一直要求登录，获取不到令牌，而且一定不能被泄露
                 .secret(passwordEncoder.encode("mengxuegu-secret"))
@@ -59,7 +70,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 // false表示需要跳转到用户授权页面手动授权，true不用手动授权，直接响应授权码
                 .autoApprove(false)
                 // 客户端回调地址
-                .redirectUris("https://www.baidu.com/");
+                .redirectUris("https://www.baidu.com/");*/
+
+        // 数据库管理
+        clients.withClientDetails(jdbcClientDetailsService());
     }
 
     /**
@@ -75,7 +89,46 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 //刷新令牌时需要
                 .userDetailsService(customUserDetailsService)
                 // 令牌管理策略配置
-                .tokenStore(tokenStore);
+                .tokenStore(tokenStore)
+                // 授权码管理策略配置
+                .authorizationCodeServices(jdbcAuthorizationCodeServices());
 
+    }
+
+    /**
+     * 令牌端点安全配置
+     *
+     * @param security
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+                // 所有人可以访问/oauth/token_key
+        security.tokenKeyAccess("permitAll()")
+                // 认证后可以访问/oauth/check_token，默认拒绝访问
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    /**
+     * 添加JDBC授权码管理策略
+     *
+     * @return
+     */
+    @Bean
+    public AuthorizationCodeServices jdbcAuthorizationCodeServices() {
+        // JDBC方式保存授权码到oauth_code表中
+        // 先保存授权码，获取令牌token后删除授权码，其实放到数据库意义不大
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+    /**
+     * 添加客户端管理策略为JDBC
+     *
+     * @return
+     */
+    @Bean
+    public ClientDetailsService jdbcClientDetailsService() {
+        // 使用JDBC 方式管理客户端信息
+        return new JdbcClientDetailsService(dataSource);
     }
 }
